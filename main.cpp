7e01f6e8c9392fdc2ba0d74b28306083a8bc5cb7
brandon <brandonwong@berkeley.edu>
2021-04-24 11:56:59 -0700

    swap main to use cpu version

--- main.cpp
+++ main.cpp
@@ -210,7 +210,10 @@ void dnaSampleRun(string refFile, string queFile, string out_file){
   short scores[] = {1, -3, -3, -1};
   ofstream results_file(out_file);
 
-  gpu_bsw_driver::kernel_driver_dna(G_sequencesB, G_sequencesA,&results_test, scores, 0.5);
+  //gpu_bsw_driver::kernel_driver_dna(G_sequencesB, G_sequencesA,&results_test, scores, 0.5);
+
+  gpu_bsw_driver::cpu_driver_dna(G_sequencesB, G_sequencesA,&results_test, scores, 0.5);
+
   for(int k = 0; k < G_sequencesA.size(); k++){
         results_file<<results_test.top_scores[k]<<"\t"
                 <<results_test.ref_begin[k]<<"\t"
