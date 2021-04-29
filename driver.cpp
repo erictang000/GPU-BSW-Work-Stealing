34c26502528fad985e7eb2c3aa2fade5355589ef
brandon <brandonwong@berkeley.edu>
2021-04-29 05:18:06 -0700

    reorganizes cpu driver to function,  gpu stub

--- driver.cpp
+++ driver.cpp
@@ -7,6 +7,7 @@
 #include <thrust/host_vector.h>
 #include <thrust/scan.h>
 #include "ssw.h"
+#include <atomic>
 
 size_t gpu_bsw_driver::get_tot_gpu_mem(int id) {
   cudaDeviceProp prop;
@@ -14,23 +15,334 @@ size_t gpu_bsw_driver::get_tot_gpu_mem(int id) {
   return prop.totalGlobalMem;
 }
 
+
+
+/* This table is used to transform nucleotide letters into numbers. */
+int8_t nt_table[128] = {
+  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+  4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+};
+
+//there really is a lot of crazy coming into this function
+// 1 read, 1 contig to be aligned
+// the resulting alignment block to point to
+// where in that block to point to (see note in function)
+// the scoring matrix for ssw, the size of that matrix for ssw (maybe pull it out into static space)
+// the start gap scoring
+// the end gap scoring
+
+void cpu_do_one_alignment(std::string read, std::string contig, gpu_bsw_driver::alignment_results *alignments, int alignment_index, const int8_t* mat, int32_t n, short startGap, short extendGap)
+{
+      int32_t s1=67108864,flag=0,filter=0;
+      int8_t* table = nt_table;
+      const int32_t current_read_length = read.length();
+      const int32_t current_contig_length = contig.length();
+
+      int8_t* current_read_numeric = (int8_t*)malloc(s1);   //this is a sore point in the code, we really want something better... i would be happier with each caller to pass in its own storage.
+      int8_t* current_contig_numeric = (int8_t*)malloc(s1); //taking values from the example, ssw usually does a realloc schme thats weird.
+
+      //just convert the data to the format ssw wants...
+      for(int i=0; i < current_read_length; ++i)
+      {
+        current_read_numeric[i] = table[(int)read[i]];
+      }
+      for(int i=0; i < current_contig_length; ++i)
+      {
+        current_contig_numeric[i] = table[(int)contig[i]];
+      }
+
+      //create a ssw profile from init
+		  s_profile* p; //, *p_rc = 0; //we don't need the reverse compliment i assume.
+      s_align* result;
+      int32_t maskLen = current_read_length/2; //following the example, this is the mask length, used for suboptimal alignments ??
+
+      //s_profile* ssw_init (const int8_t* read, const int32_t readLen, const int8_t* mat, const int32_t n, const int8_t score_size);
+      // score_size = 2 means i dont know,,, 0 < 255, 1 >= 255, both are meaningless to me.
+      p = ssw_init(current_read_numeric,current_read_length,mat,n,2);
+			result = ssw_align (p, current_contig_numeric, current_contig_length, startGap * -1, extendGap * -1, flag, filter, 0, maskLen);
+
+      //assign the results to the input array, no one shares an alignment index so it should be thread safe....
+      //the way the result data structure points to more arrays of individual results makes it difficult to 
+      //have this function just return a single result, and let the caller handle it, this function should be
+      //more attached to the alignment results data, in either event passint it in is super weird, but this is the explanation.
+      alignments->query_begin[alignment_index] = result->ref_begin1;
+      alignments->query_end[alignment_index] = result->ref_end1;
+      alignments->ref_begin[alignment_index] = result->read_begin1;
+      alignments->ref_end[alignment_index] = result->read_end1;
+      alignments->top_scores[alignment_index] = result->score1;
+
+      //destroy the result since ssw_init and ssw_align allocates something..
+      align_destroy(result);
+      init_destroy(p);
+
+      free(current_read_numeric);
+      free(current_contig_numeric);
+}
+
+//strA and strB are cuda allocated storage for all the strings in the batch..., we don't want to re-malloc them for every batch
+//offsetA_h and offsetB_h are also cuda mallocs on the host to store i think results...
+
+// NOTE: this might not quite work as a function because we need to do some stealing and this hides the queue from the process...
+//       it can be here for now until i get the single pull from the cpu working well then we can maybe shimmy this in well...
+/**
+void gpu_do_batch_alignments(std::vector<std::string> sequencesA, std::vector<std::string> sequencesB, int batch_size, gpu_bsw_driver::alignment_results *alignments, int alignment_index, char*strA, char*strB,char *strA_d, char *strB_d,unsigned* offsetA_h, unsigned* offsetB_h, unsigned int maxContigSize, unsigned int maxReadSize, cudaStream_t* streams_cuda)
+{
+    auto packing_start = NOW;
+
+    //pointers to where in the alignment results we are going to return into...
+    short* alAbeg = alignments->ref_begin + alignment_index;
+    short* alBbeg = alignments->query_begin + alignment_index;
+    short* alAend = alignments->ref_end + alignment_index;
+    short* alBend = alignments->query_end + alignment_index;  // memory on CPU for copying the results
+    short* top_scores_cpu = alignments->top_scores + alignment_index;
+
+    gpu_alignments gpu_data(batch_size); //i guess this cleans itself up when it leaves scope...
+
+    int blocksLaunched = batch_size; //0;
+    // std::vector<std::string>::const_iterator beginAVec;
+    // std::vector<std::string>::const_iterator endAVec;
+    // std::vector<std::string>::const_iterator beginBVec;
+    // std::vector<std::string>::const_iterator endBVec;
+
+    // std::vector<std::string> sequencesA(beginAVec, endAVec);
+    // std::vector<std::string> sequencesB(beginBVec, endBVec);  //we can just pass in seqA and B if this is where things live at the end of the day
+
+    long unsigned running_sum = 0;
+    int sequences_per_stream = (blocksLaunched) / NSTREAMS;
+    int sequences_stream_leftover = (blocksLaunched) % NSTREAMS;
+    long unsigned half_length_A = 0;
+    long unsigned half_length_B = 0;
+
+    auto start_cpu = NOW;
+
+    for(int i = 0; i < sequencesA.size(); i++)
+    {
+        running_sum +=sequencesA[i].size();
+        offsetA_h[i] = running_sum;//sequencesA[i].size();
+        if(i == sequences_per_stream - 1){
+            half_length_A = running_sum;
+            running_sum = 0;
+          }
+    }
+    long unsigned totalLengthA = half_length_A + offsetA_h[sequencesA.size() - 1];
+
+    running_sum = 0;
+    for(int i = 0; i < sequencesB.size(); i++)
+    {
+        running_sum +=sequencesB[i].size();
+        offsetB_h[i] = running_sum; //sequencesB[i].size();
+        if(i == sequences_per_stream - 1){
+          half_length_B = running_sum;
+          running_sum = 0;
+        }
+    }
+    long unsigned totalLengthB = half_length_B + offsetB_h[sequencesB.size() - 1];
+
+    auto end_cpu = NOW;
+    std::chrono::duration<double> cpu_dur = end_cpu - start_cpu;
+
+    //total_time_cpu += cpu_dur.count();
+    long unsigned offsetSumA = 0;
+    long unsigned offsetSumB = 0;
+
+    //BW NOTE: strA and strB live in host memory, per thread, this is copying the sequences to there.
+    //         sequencesA and sequencesB was constructed at the beginning of the loop, it is all the 
+    //         query/ref sequences per iteration...
+
+    for(int i = 0; i < sequencesA.size(); i++)
+    {
+        char* seqptrA = strA + offsetSumA;
+        memcpy(seqptrA, sequencesA[i].c_str(), sequencesA[i].size());
+        char* seqptrB = strB + offsetSumB;
+        memcpy(seqptrB, sequencesB[i].c_str(), sequencesB[i].size());
+        offsetSumA += sequencesA[i].size();
+        offsetSumB += sequencesB[i].size();
+    }
+
+    auto packing_end = NOW;
+    std::chrono::duration<double> packing_dur = packing_end - packing_start;
+
+    //total_packing += packing_dur.count();
+
+    asynch_mem_copies_htd(&gpu_data, offsetA_h, offsetB_h, strA, strA_d, strB, strB_d, half_length_A, half_length_B, totalLengthA, totalLengthB, sequences_per_stream, sequences_stream_leftover, streams_cuda);
+
+
+    //BW NOTE: minSize is the lesser of the biggest query or reference string.
+    unsigned minSize = (maxReadSize < maxContigSize) ? maxReadSize : maxContigSize;
+    unsigned totShmem = 3 * (minSize + 1) * sizeof(short);
+    unsigned alignmentPad = 4 + (4 - totShmem % 4);
+    size_t   ShmemBytes = totShmem + alignmentPad;
+    if(ShmemBytes > 48000)
+        cudaFuncSetAttribute(gpu_bsw::sequence_dna_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, ShmemBytes);
+
+    gpu_bsw::sequence_dna_kernel<<<sequences_per_stream, minSize, ShmemBytes, streams_cuda[0]>>>(
+        strA_d, strB_d, gpu_data.offset_ref_gpu, gpu_data.offset_query_gpu, gpu_data.ref_start_gpu,
+        gpu_data.ref_end_gpu, gpu_data.query_start_gpu, gpu_data.query_end_gpu, gpu_data.scores_gpu, matchScore, misMatchScore, startGap, extendGap);
+    cudaErrchk(cudaGetLastError());
+
+    gpu_bsw::sequence_dna_kernel<<<sequences_per_stream + sequences_stream_leftover, minSize, ShmemBytes, streams_cuda[1]>>>(
+        strA_d + half_length_A, strB_d + half_length_B, gpu_data.offset_ref_gpu + sequences_per_stream, gpu_data.offset_query_gpu + sequences_per_stream,
+          gpu_data.ref_start_gpu + sequences_per_stream, gpu_data.ref_end_gpu + sequences_per_stream, gpu_data.query_start_gpu + sequences_per_stream, gpu_data.query_end_gpu + sequences_per_stream,
+          gpu_data.scores_gpu + sequences_per_stream, matchScore, misMatchScore, startGap, extendGap);
+    cudaErrchk(cudaGetLastError());
+
+    // copyin back end index so that we can find new min
+    asynch_mem_copies_dth_mid(&gpu_data, alAend, alBend, sequences_per_stream, sequences_stream_leftover, streams_cuda);
+
+    //this also saves out the results of the end locations... into alAend and alBend
+
+    cudaStreamSynchronize (streams_cuda[0]);
+    cudaStreamSynchronize (streams_cuda[1]);
+
+    auto sec_cpu_start = NOW;
+    int newMin = get_new_min_length(alAend, alBend, blocksLaunched); // find the new largest of smaller lengths
+    auto sec_cpu_end = NOW;
+    std::chrono::duration<double> dur_sec_cpu = sec_cpu_end - sec_cpu_start;
+    //total_time_cpu += dur_sec_cpu.count();
+
+    gpu_bsw::sequence_dna_reverse<<<sequences_per_stream, newMin, ShmemBytes, streams_cuda[0]>>>(
+            strA_d, strB_d, gpu_data.offset_ref_gpu, gpu_data.offset_query_gpu, gpu_data.ref_start_gpu,
+            gpu_data.ref_end_gpu, gpu_data.query_start_gpu, gpu_data.query_end_gpu, gpu_data.scores_gpu, matchScore, misMatchScore, startGap, extendGap);
+    cudaErrchk(cudaGetLastError());
+
+    gpu_bsw::sequence_dna_reverse<<<sequences_per_stream + sequences_stream_leftover, newMin, ShmemBytes, streams_cuda[1]>>>(
+            strA_d + half_length_A, strB_d + half_length_B, gpu_data.offset_ref_gpu + sequences_per_stream, gpu_data.offset_query_gpu + sequences_per_stream ,
+            gpu_data.ref_start_gpu + sequences_per_stream, gpu_data.ref_end_gpu + sequences_per_stream, gpu_data.query_start_gpu + sequences_per_stream, gpu_data.query_end_gpu + sequences_per_stream,
+            gpu_data.scores_gpu + sequences_per_stream, matchScore, misMatchScore, startGap, extendGap);
+    cudaErrchk(cudaGetLastError());
+
+    //this copies the results from gpu_data out to a pointer of where the A and B results have their "start" query but also named beg here.
+    asynch_mem_copies_dth(&gpu_data, alAbeg, alBbeg, top_scores_cpu, sequences_per_stream, sequences_stream_leftover, streams_cuda);
+
+}
+**/
+
+//struct vs std::pair? 
+// typedef struct alignment_batch_t{
+//     int start;
+//     int stop;
+// } alignment_batch_t;
+
+// typedef struct AtomicAlignmentQueue{
+//   std::atomic_uint64_t current_index;
+//   int max_index;
+//   AtomicAlignmentQueue() : current_index(0){}
+//   alignment_batch_t GetBatch(int count){ alignment_batch_t retVal; retVal.start = current_index.fetch_add(count); retVal.stop = retVal.start + count;};
+// } AtomicAlignmentQueue;
+
+
+//note: OMP Atomic protects a single variable OMP Critical can protect an entire block...
+
+void
+gpu_bsw_driver::gpu_cpu_driver_dna(std::vector<std::string> reads, std::vector<std::string> contigs, gpu_bsw_driver::alignment_results *alignments, short scores[4], float factor)
+{
+    //initialize some values from the original implementation.
+    int32_t l,m,k,n=5;
+    short matchScore = scores[0], misMatchScore = scores[1], startGap = scores[2], extendGap = scores[3];
+    unsigned maxContigSize = getMaxLength(contigs);
+    unsigned maxReadSize = getMaxLength(reads);
+    unsigned totalAlignments = contigs.size(); // assuming that read and contig vectors are same length 
+    // not sure actual intention, if we just want the max(reads,contigs) that is not hard but... all tests we have are of same length
+    auto read_sequence_ptr = reads.begin();
+    auto contig_sequence_ptr = contigs.begin();
+    initialize_alignments(alignments, totalAlignments); // pinned memory allocation
+
+    //bw: ssw uses a 5x5 scoring matrix ?!?! maybe for "ambiguous base?"
+    //  but the header uses a 4x4 scoring matrix in the example. we will use the demo version.
+    int8_t* mata = (int8_t*)calloc(25, sizeof(int8_t));
+    const int8_t* mat = mata;
+
+    // initialize scoring matrix for genome sequences --- more copy pasta, 
+    for (l = k = 0; (l < 4); ++l) {
+      for (m = 0; (m < 4); ++m) mata[k++] = l == m ? matchScore : misMatchScore;
+      mata[k++] = 0; // ambiguous base
+    }
+
+    int batch_size = 1; //different for gpu/cpu?
+
+    std::cout<< "SSW GPU-CPU DRIVER STARTED w/" << omp_get_max_threads() << " threads!" << std::endl;
+  
+    //shared variables, should also only be touched in atomic or critical regions...
+    uint64_t work_stolen_count=0;
+    uint64_t total_work_alignment_index=0;
+
+    //creates a parallel region, explicitly stating the variables we want to be shared.
+    #pragma omp parallel shared(work_stolen_count,total_work_alignment_index)
+    {
+      #pragma omp atomic read
+      uint64_t atomic_alignment_index = total_work_alignment_index;
+
+      while(atomic_alignment_index < totalAlignments)
+      {
+
+        //********* CPU THREAD WORK
+        
+        int thread_current_alignment_index_start;
+        int thread_current_alignment_index_end;
+        //eat up the alignments until the queue is completed.
+        //#pragma omp atomic capture
+        #pragma omp critical //maybe doing too much for atomic, just use critical for now.
+        { 
+          thread_current_alignment_index_start=total_work_alignment_index; 
+          if(thread_current_alignment_index_start + batch_size < totalAlignments)
+          {
+            thread_current_alignment_index_end = total_work_alignment_index+=batch_size;
+          }
+          else
+          {
+            //take the last "batch"
+            total_work_alignment_index = totalAlignments;
+            thread_current_alignment_index_end = totalAlignments;
+
+            if(thread_current_alignment_index_start >= totalAlignments)
+            {
+              //something bad happened, abort. that while loop might not be thread safe?
+              std::cout << "Warning Atomic Queue is Broken!" << std::endl;
+              //assert
+            }
+          }
+          
+        }
+
+        for(int i=thread_current_alignment_index_start; i < thread_current_alignment_index_end; ++i)
+        {
+
+          auto  current_read = *(read_sequence_ptr+i);
+          auto  current_contig = *(contig_sequence_ptr+i);
+
+          cpu_do_one_alignment(current_read,current_contig,alignments,i,mat,n,startGap,extendGap);
+        }
+        //*********
+
+
+        //TODO DO GPU THREAD WORK, this will pull a batch from the queue but also do the whole cpu thread work routine, between kernel calls
+        // to start lets not do that, it might not actually be worth it anyway... just assume rank=0 owns a gpu and only does gpu work.
+
+
+        #pragma omp atomic read
+        atomic_alignment_index = total_work_alignment_index;
+      }
+    }
+
+    free(mata);
+
+}
+
+
+
 void
 gpu_bsw_driver::cpu_driver_dna(std::vector<std::string> reads, std::vector<std::string> contigs, gpu_bsw_driver::alignment_results *alignments, short scores[4], float factor)
 {
 
     std::cout<< "SSW CPU DRIVER STARTED w/" << omp_get_max_threads() << " threads!" << std::endl;
 
-    /* This table is used to transform nucleotide letters into numbers. */
-    int8_t nt_table[128] = {
-      4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
-      4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
-    };
+
 
   	int8_t* table = nt_table;
 
