16aff22ba735e1adc6ce590a409f5f8e6d9d1703
brandon <brandonwong@berkeley.edu>
2021-04-29 05:18:40 -0700

    missed files.

--- driver.hpp
+++ driver.hpp
@@ -36,6 +36,8 @@ kernel_driver_dna(std::vector<std::string> reads, std::vector<std::string> conti
 void
 cpu_driver_dna(std::vector<std::string> reads, std::vector<std::string> contigs, gpu_bsw_driver::alignment_results *alignments, short scores[4], float factor = 1.0);
 
+void
+gpu_cpu_driver_dna(std::vector<std::string> reads, std::vector<std::string> contigs, gpu_bsw_driver::alignment_results *alignments, short scores[4], float factor);
 
 void
 kernel_driver_aa(std::vector<std::string> reads, std::vector<std::string> contigs, alignment_results *alignments, short scoring_matrix[], short openGap, short extendGap, float factor = 1.0);
