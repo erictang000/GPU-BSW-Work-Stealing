dfb4b42df2b7aa270132b41516c6435f78ae05c1
brandon <brandonwong@berkeley.edu>
2021-05-02 10:26:13 -0700

    no steal limit. last test.

--- driver.cpp
+++ driver.cpp
@@ -394,7 +394,7 @@ gpu_bsw_driver::gpu_cpu_driver_dna(std::vector<std::string> reads, std::vector<s
         atomic_alignment_index = total_work_alignment_index;
 
         //CPU WORK LIMIT... the cpu should not try to do work as we near the end...maybe?
-        int CPU_LIMIT = 500000;
+        int CPU_LIMIT = 0;
 
         while(atomic_alignment_index < (totalAlignments-CPU_LIMIT))
         {
