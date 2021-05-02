b18637359e50e8ccc85cabd3d3eec631993e3112
brandon <brandonwong@berkeley.edu>
2021-05-02 08:05:45 -0700

    lower the batch size to 10 for CPUs

--- driver.cpp
+++ driver.cpp
@@ -272,7 +272,7 @@ gpu_bsw_driver::gpu_cpu_driver_dna(std::vector<std::string> reads, std::vector<s
       mata[k++] = 0; // ambiguous base
     }
 
-    int batch_size = 100; //different for gpu/cpu?
+    int batch_size = 10; //different for gpu/cpu?
 
     auto start = NOW;
 
